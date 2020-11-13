1f5d5c1e39c06ae19f4610e5ff7e889b2178bef6
sheptianbagja <sheptianbagjautama@gmail.com>
2020-11-14 00:26:30 +0700

    Integrasi api chapters dengan api gateway

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const ordersRouter  = require('./routes/orders');
 const paymentsRouter  = require('./routes/payments');
 const refreshTokensRouter  = require('./routes/refreshTokens');
 const mentorsRouter = require('./routes/mentors');
+const chaptersRouter = require('./routes/chapters');
 
 
 const verifyToken = require('./middlewares/verifyToken');
@@ -32,5 +33,6 @@ app.use('/orders', ordersRouter);
 app.use('/payments', paymentsRouter);
 app.use('/refresh-tokens', refreshTokensRouter);
 app.use('/mentors', verifyToken ,mentorsRouter);
+app.use('/chapters' , verifyToken, chaptersRouter);
 
 module.exports = app;
